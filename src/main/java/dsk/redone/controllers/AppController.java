9e2a57a03ba3b890b017f65225b951a1895413e6
Vladimir Vavin <151967675+DeepSkyKeeper@users.noreply.github.com>
2024-04-08 14:06:17 +0300

    Update AppController.java
    delete choose view

--- src/main/java/dsk/redone/controllers/AppController.java
+++ src/main/java/dsk/redone/controllers/AppController.java
@@ -37,8 +37,7 @@ public class AppController {
         curId = repo.getIdByCurName(curName);
         //поиск id по названию валюты в репозитории
 
-        //выбираем источник для вывода
-//        inputController.chooseOutputType();
+    
         //ввод даты
         //парсим строку - конвертим в дату
         //создаем доп даты
